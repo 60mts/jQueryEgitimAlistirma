06d7ab9f55d6865896e3604bec06aa192b2c12dc
ᴍᴜʜᴀᴍᴍᴇᴅ ᴛᴀʏʏɪʙ Şᴀʜɪɴ <62522168+60mts@users.noreply.github.com>
2023-04-05 20:14:06 +0300

    main

---
+++ ders6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<input type="text" name=""placeholder="UserName"> 
+<br>
+<br>
+<input type="password" name="" placeholder="Password">
+
+
+<link src="bootstrap/node_modules/bootstrap/dist/css/bootstrap.css">
+<script src="js/node_modules/jquery/dist/jquery.js"></script>
+<script src="js/node_modules/jquery/dist/index.js"></script>
+</body>
+</html>
