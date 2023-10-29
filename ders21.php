98e240de407b1f21c7b21c1f72ff22a82bfd613b
Muhammed Tayyib Şahin <muhammedtayyibsahin@yandex.com>
2023-10-29 17:09:34 +0300

    main

---
+++ ders21.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<input type="text" placeholder="Mario Icardi">
+
+
+
+
+<link src="bootstrap/node_modules/bootstrap/dist/css/bootstrap.css">
+<script src="js/node_modules/jquery/dist/jquery.js"></script>
+<script src="js/node_modules/jquery/dist/index.js"></script>
+</body>
+</html>
